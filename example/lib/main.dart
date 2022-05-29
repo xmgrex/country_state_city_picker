e03fc245028274c928964985eabff5cbfb8c7c50
kyouyagenki <rebertas.g@ezweb.ne.jp>
2022-05-29 17:26:06 +0900

    create example

---
+++ example/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:country_state_city_picker/country_state_city_picker.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SelectState(
+        onCountryChanged: (v) {},
+        onStateChanged: (v) {},
+        onCityChanged: (v) {},
+      ),
+    );
+  }
+}
